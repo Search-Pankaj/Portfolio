d46c088013fa0607ba8279171d0ac607b5bd7b5b
Search-Pankaj <pankajmay97@gmail.com>
2021-06-16 13:13:56 +0530

    added navbar and info section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,64 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { DiCodeBadge, DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Span,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container style={{ marginTop: "2rem" }}>
+    <Div1>
+      <Link href="/">
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "2rem",
+          }}
+        >
+          <DiCssdeck size="3rem" />{" "}
+          <span style={{ fontSize: "2rem" }}>Pankaj Bhardwaj</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#Project">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#Skill">
+          <NavLink>Skill</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#About">
+          <NavLink>Contact Me</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/Search-Pankaj" target="_blank">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://github.com/Search-Pankaj" target="_blank">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://github.com/Search-Pankaj" target="_blank">
+        <DiCodeBadge size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hi! <br />
+          Welcome To my Website
+        </SectionTitle>
+        <SectionText></SectionText>
+        <Button onClick={props.handleClick}>Download CV</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
