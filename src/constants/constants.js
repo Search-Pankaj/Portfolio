0278a4481c9f8c2060530adae930377f2918a9ca
Search-Pankaj <pankajmay97@gmail.com>
2021-06-23 17:17:30 +0530

    added project detail

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "NOWORRY",
     description:
-      "Designed and developed a web application with backend on Node.js and Express.js. Used Notion API to Fetch Notion App Data and displayed it in our App. Allowed more than 60 student to stayed update with Upcoming Event and Assignment  Used HTML, CSS and Bootstrap to have user-friendly front end ",
+      "Allowed more than 60 student to stayed update with Upcoming Event and Assignment  Used HTML, CSS and Bootstrap to have user-friendly front end ",
     image: "/images/1.png",
     tags: ["Node.js", "Express", "Notion API", "Bootstrap"],
     source: "https://github.com/Search-Pankaj/Noworry",
@@ -12,7 +12,7 @@ export const projects = [
   {
     title: "SYSTEM MONITOR",
     description:
-      "Developed a web application that perform like windows task manager with good UI. It Display Real-Time CPU and RAM usage and more detail about Processor.Keep record of previous performance stats, which make it easy to analysis performance ",
+      "Developed a web application that perform like windows task manager with good UI. It Display Real-Time CPU and RAM  ",
     image: "/images/2.png",
     tags: ["React", "JavaScript"],
     source: "https://github.com/Search-Pankaj/System_Monitor",
@@ -22,21 +22,12 @@ export const projects = [
   {
     title: "KONECT",
     description:
-      "Developed Video-Chat application similar to zoom and Google meet.Used Node.js and Express and Socket.io on Backend, Deployed on Heroku.Used React.js, Material Ui ,Socket-io-client on frontend, Deployed on Netlify.Allow User to Connect to their friend and Video Chat without Registration ",
+      "Developed Video-Chat application similar to zoom and Google meet.Allow User to Connect to their friend and Video Chat without Registration ",
     image: "/images/3.jpg",
     tags: ["React", "WebRTC", "Node.js", "Socket.io"],
     source: "https://github.com/Search-Pankaj/konect",
     visit: "https://konect.netlify.app/",
     id: 2,
-  },
-  {
-    title: "Portfolio ",
-    description: "This is my portfolio ",
-    image: "/images/4.jpg",
-    tags: ["React", "Material ui", "Next.js"],
-    source: "https://github.com/Search-Pankaj/Portfolio",
-    visit: "https://searchpankaj.netlify.app/",
-    id: 3,
   },
 ];
 
